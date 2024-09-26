3047f74d0a0cf022b21fac09cd0a2dc493813feb
Pavlenco05 <pasapavl.09102030@gmail.com>
2024-09-27 01:26:26 +0300

    Initial commit with Next.js setup and project structure

---
+++ ecommerce-store/app/components/navbar.tsx
@@ -0,0 +1,18 @@
+import Container from '@/components/ui/container';
+
+const Navbar = () => {
+    return (
+        <div className="border-b">
+            <Container>
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+            <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                <p className="font-bold text-xl">STORE</p>
+            </Link>
+            <MainNav data={categories} />
+            </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ ecommerce-store/app/layout.tsx
@@ -0,0 +1,27 @@
+import { Urbanist } from 'next/font/google'
+
+import Footer from '@/components/footer' // Updated to lowercase 'footer'
+
+import Navbar from '@/components/navbar' // Updated to lowercase 'navbar'
+import './globals.css'
+const font = Urbanist({ subsets: ['latin'] })
+export const metadata = {
+  title: 'Store',
+  description: 'Store',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar/>
+        {children}
+      <Footer />
+      </body> 
+    </html>
+  )
+}
